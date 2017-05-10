2d906c3161f7533ffeff8d3d7eaaf9438ad1ebaa
Jennifer Endisch <Jennifer Endisch>
2017-05-10 16:49:06 -0500

    Done with 5+ tests for each Add/Remove methods that all pass

---
+++ CustomList/CustomList/List.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class List<T> : IEnumerable
+    {
+
+        int length;
+        public T[] innerArray;
+        bool trueOrFalse;
+
+        public List()
+        {
+            length = 0;
+            innerArray = new T[length];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return length;
+            }
+            private set { }
+        }
+
+        public int Capacity()
+        {
+            int count = 0;
+            for (int i = 0; i < innerArray.Length; i++)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                return innerArray[CheckTheIndex(i)];
+            }
+
+            set
+            {
+                innerArray[CheckTheIndex(i)] = value;
+            }
+        }
+        
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < innerArray.Length; i++)
+            {
+                yield return innerArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            T[] newInnerArray = new T[length + 1];
+
+            for (int i = 0; i < length; i++)
+            {
+                newInnerArray[i] = innerArray[i];
+            }
+            newInnerArray[length] = item;
+
+            innerArray = newInnerArray;
+            length++;
+        }
+
+        public bool Remove(T item)
+        {
+            T[] newInnerArray = new T[length - 1];
+            trueOrFalse = false;
+            for (int i = 0; i < length - 1; i++)
+            {
+                trueOrFalse = FindEqualItem(item, i);
+                if (trueOrFalse == true)
+                {
+                    length--;
+                    SetNewListAfterRemovedItemFound(i, newInnerArray);
+                    newInnerArray[i] = innerArray[i + 1];
+                    break;
+                }
+                else
+                {
+                    newInnerArray[i] = innerArray[i];
+                }
+            }
+            innerArray = newInnerArray;
+            return trueOrFalse;
+        }
+
+        public void SetNewListAfterRemovedItemFound(int i, T[]newInnerArray)
+        {
+            for (int j = i; j < length; j++)
+            {
+                newInnerArray[j] = innerArray[j + 1];
+            }
+        }
+
+        public bool FindEqualItem(T item, int i)
+        {
+
+            if (innerArray[i].Equals(item))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int CheckTheIndex(int i)
+        {
+            if (i >= 0 && i < length)
+            {
+                return i;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("i");
+            }
+        }
+
+
+
+}
+}
---
+++ CustomList/CustomListTests/UnitTest1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        //Tests for int
+
+
+        //[TestMethod]
+        //public void Count_CountNumberOfObjectsInList_ReturnNumberOfItems()
+        //{
+        //    List<int> list = new List<int>();
+        //    //Arrange
+        //    int result;
+        //    int expected = 0;
+
+        //    //Act
+        //    result = list.Count;
+
+        //    //Assert
+        //    Assert.AreEqual(result, expected);
+        //}
+
+        //[TestMethod]
+        //public void Add_AddObjectToList_ReturnLengthOfListWithAddedElement()
+        //{
+        //    List<int> list = new List<int>();
+        //    // Arrange
+        //    int number = 3;
+        //    int expected = 1;
+        //    int result;
+
+        //    // Act
+        //    list.Add(number);
+        //    result = list.Count;
+
+        //    // Assert
+        //    Assert.AreEqual(expected, result);
+        //}
+
+        //[TestMethod]
+        //public void Add_AddFirstObjectToList_ReturnsSameObjectThatWasAdded()
+        //{
+        //    List<int> list = new List<int>();
+        //    // Arrange
+        //    int number = 3;
+
+        //    // Act
+        //    list.Add(number);
+
+        //    // Assert
+        //    Assert.AreEqual(number, list[0]);
+        //}
+
+        //[TestMethod]
+        //public void Add_AddTwoObjectsToList_ReturnsFirstObjectThatWasAdded()
+        //{
+        //    List<int> list = new List<int>();
+        //    // Arrange
+        //    int number = 1;
+        //    int biggernumber = 3;
+
+        //    // Act
+        //    list.Add(number);
+        //    list.Add(biggernumber);
+
+        //    // Assert
+        //    Assert.AreEqual(number, list[0]);
+        //}
+
+        //[TestMethod]
+        //public void Add_AddTwoObjectsToList_ReturnsSecondObjectThatWasAdded()
+        //{
+        //    List<int> list = new List<int>();
+        //    // Arrange
+        //    int number = 1;
+        //    int biggernumber = 3;
+
+        //    // Act
+        //    list.Add(number);
+        //    list.Add(biggernumber);
+
+        //    // Assert
+        //    Assert.AreEqual(biggernumber, list[1]);
+        //}
+
+        //[TestMethod]
+        //public void Add_AddingMultipleObjectsToList_ReturnLengthOfListWithAddedElements()
+        //{
+        //    // Arrange
+        //    List<int> list = new List<int>();
+        //    int number = 1;
+        //    int biggerNumber = 2;
+        //    int biggestNumber = 3;
+        //    int expected = 3;
+        //    int result;
+
+        //    // Act
+        //    list.Add(number);
+        //    list.Add(biggerNumber);
+        //    list.Add(biggestNumber);
+        //    result = list.Count;
+
+        //    // Assert
+        //    Assert.AreEqual(expected, result);
+        //}
+
+        //[TestMethod]
+        //public void Add_AddStringToList_CheckAndSeeIfStringAndFirstPositionMatch()
+        //{
+        //    List<string> list = new List<string>();
+        //    // Arrange
+        //    string greeting = "Hello World!";
+
+        //    // Act
+        //    list.Add(greeting);
+
+        //    // Assert
+        //    Assert.AreEqual(greeting, list[0]);
+        //}
+
+        //[TestMethod]
+        //public void Remove_RemoveAnObjectFromAList_ReturnBool()
+        //{
+        //    List<int> list = new List<int>() { 1, 2, 3 };
+        //    // Arrange
+        //    int number = 2;
+        //    bool result;
+
+        //    // Act
+        //    result = list.Remove(number);
+
+        //    // Assert
+        //    Assert.IsTrue(result);
+        //}
+
+        //[TestMethod]
+        //public void Remove_RemoveAnObjectThatIsntThereFromAList_ReturnBool()
+        //{
+        //    List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+        //    // Arrange
+        //    int number = 8;
+        //    bool result;
+
+        //    // Act
+        //    result = list.Remove(number);
+
+        //    // Assert
+        //    Assert.IsFalse(result);
+        //}
+
+        //[TestMethod]
+        //public void Remove_RemoveCertainObjectFromList_ReturnListWithRemovedObject()
+        //{
+        //    List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+        //    // Arrange
+        //    int number = 5;
+
+        //    // Act
+        //    list.Remove(number);
+
+        //    // Assert
+        //    Assert.AreEqual(1, list[0]);
+        //    Assert.AreEqual(2, list[1]);
+        //    Assert.AreEqual(3, list[2]);
+        //    Assert.AreEqual(4, list[3]);
+        //}
+
+        [TestMethod]
+        public void Remove_RemoveCertainObjectMultipleFromList_ReturnListWithRemovedObject()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 1 };
+            // Arrange
+            int number = 1;
+
+            // Act
+            list.Remove(number);
+
+            // Assert
+            Assert.AreEqual(2, list[0]);
+            Assert.AreEqual(3, list[1]);
+            Assert.AreEqual(4, list[2]);
+            Assert.AreEqual(1, list[3]);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveCertainObjectMultipleFromList_ReturnListCountWithRemovedObject()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 1 };
+            // Arrange
+            int number = 1;
+            int expected = 4;
+            int result;
+
+            // Act
+            list.Remove(number);
+            result = list.Capacity();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        //[TestMethod]
+        //public void Remove_RemoveObjectFromList_ReturnLengthOfListWithRemovedElement()
+        //{
+        //    List<int> list = new List<int>();
+        //    //Arrange
+        //    int number = 3;
+        //    int expected = 0;
+        //    int result;
+
+        //    //Act
+        //    list.Add(number);
+        //    list.Remove(number);
+        //    result = list.Capacity();
+
+        //    //Assert
+        //    Assert.AreEqual(expected, result);
+        //}
+
+        //[TestMethod]
+        //public void Remove_RemoveMultipleObjectsFromList_ReturnLengthOfListWithRemovedElements()
+        //{
+        //    List<int> list = new List<int>();
+        //    //Arrange
+        //    int number = 1;
+        //    int biggerNumber = 2;
+        //    int biggestNumber = 3;
+        //    int expected = 1;
+        //    int result;
+
+        //    //Act
+        //    list.Add(number);
+        //    list.Add(biggerNumber);
+        //    list.Add(biggestNumber);
+        //    list.Remove(biggerNumber);
+        //    list.Remove(biggestNumber);
+        //    result = list.Capacity();
+
+        //    //Assert
+        //    Assert.AreEqual(expected, result);
+        //}
+    }
+}
+
+
+    
